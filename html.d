9cc4165200962df05761f0c6387763e2dee44f63
DAVID MACUIXTLE VELAZQUEZ 3-W <a.23308051281192@cbtis128.edu.mx>
2024-11-06 16:36:36 -0600

    Create html.d

---
+++ html.d
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+
+  h1 {
+    
+    border: 2px solid powderblue;
+    margin: 50px;
+    color:blue;
+    font-family: Arial;
+    font-size: 300%;
+
+  }
+
+
+</style>
+</head>
+<body>
+<h1>Hello word</h1>
+
+
+
+</body>
+</html>
+![image](https://github.com/user-attachments/assets/bb616187-f58d-4c49-abfc-2539eeedaa3e)
